b9985ad5dff80ee234d3c44eb73f4ad1e1a19abc
AyaTouicha <117740707+aya2705@users.noreply.github.com>
2025-05-25 00:56:19 +0100

    Update urls.py

--- LocationVoitures/manager/urls.py
+++ LocationVoitures/manager/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from . import views
+from .views import create_manager_view,get_all_managers,delete_manager,update_manager
 
 urlpatterns = [
-    path('create/', views.create_manager, name='create_manager'),
-    path('<str:user_id>/', views.get_manager, name='get_manager'),
-    path('update/<str:user_id>/', views.update_manager, name='update_manager'),
-    path('delete/<str:user_id>/', views.delete_manager, name='delete_manager'),
-    path('', views.get_all_managers, name='get_all_managers'),
+    path('create/', create_manager_view, name='create_manager_view'),
+    path('', get_all_managers, name='get_all_managers'),
+    path('delete/<str:manager_id>/', delete_manager, name='delete_manager'),
+    path('update/<str:manager_id>/', update_manager, name='update_manager'),
 ]
